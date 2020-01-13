9b5fb9498ddd274b9b6a0cd19832d2d511985fcb
League Student <league@iMac-34.attlocal.net>
2020-01-12 17:27:19 -0800

    MoreAndMoreTests, SearchingAndSortingAlgotithms (IP), Algorithms(IP)

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -4,6 +4,82 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
+		for(int i = 0; i < eggs.size(); i++) {
+			if(eggs.get(i).contentEquals("cracked")) {
+				return i;
+			}
+		}
 		return 0;
 	}
+
+	public static Object countPearls(List<Boolean> oysters) {
+		int h = 0;
+		for(int i = 0; i < oysters.size(); i++) {
+			if(oysters.get(i).equals(true)) {
+				h+=1;
+			}
+		}
+		return h;
+	}
+
+	public static double findTallest(List<Double> peeps) {
+		double h = Integer.MIN_VALUE;
+		for(int i = 0; i < peeps.size(); i++) {
+			if(peeps.get(i) > h) {
+				h = peeps.get(i);
+			}
+		}
+		return h;
+	}
+
+	public static Object findLongestWord(List<String> words) {
+		int length = 0;
+		String longestWord = "";
+		for(int i = 0; i < words.size(); i++) {
+			if(words.get(i).length() > length) {
+				length = words.get(i).length();
+				longestWord = words.get(i);
+			}
+		}
+		return longestWord;
+	}
+	
+	public static Object containsSOS1(List<String> message1) {
+		boolean sos = false;
+		for(int i = 0; i < message1.size(); i++) {
+			if(message1.get(i).contains("... --- ...")) {
+				sos = true;
+			}
+		}
+		return sos;
+	}
+	
+	public static Object containsSOS(List<String> message2) {
+		boolean sos = false;
+		for(int i = 0; i < message2.size(); i++) {
+			if(message2.get(i).contains("... --- ...")) {
+				sos = true;
+			}
+		}
+		return sos;
+	}
+
+	public static List<String> sortScores(List<Double> results) {
+		int score = 0;
+		if(score < results.get(i)) {
+			
+		}
+		
+		
+		
+		double j = Integer.MIN_VALUE;
+		for(int i = 0; i < results.size(); i++) {
+			if(results.get(i) > j) {
+				j = results.get(i);
+			}
+		}
+		return j;
+	}
+	
+	
 }
--- src/_02_More_Algorithms/_00_MoreAndMoreTests.java
+++ src/_02_More_Algorithms/_00_MoreAndMoreTests.java
@@ -3,11 +3,9 @@ package _02_More_Algorithms;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
 /**
  * Uncomment each test and then write the method to make it pass.
  * **/
-
 public class _00_MoreAndMoreTests {
 	@Test
 	  public void MultTest() {
@@ -23,47 +21,61 @@ public class _00_MoreAndMoreTests {
 	    assertFalse(isPrime(4));
 	    assertFalse(isPrime(12));
 	    assertFalse(isPrime(527));
+	}
+	@Test
+	  public void SquareTest() {
+	    assertTrue(isSquare(4));
+	    assertTrue(isSquare(144));
+	    assertTrue(isSquare(64));
+	    assertTrue(isSquare(10201));
+	    assertTrue(isSquare(1));
+	    assertFalse(isSquare(3));
+	    assertFalse(isSquare(22));
+	    assertFalse(isSquare(143));
 	  }
-//
-//	@Test
-//	  public void SquareTest() {
-//
-//	    assertTrue(isSquare(4));
-//	    assertTrue(isSquare(144));
-//	    assertTrue(isSquare(64));
-//	    assertTrue(isSquare(10201));
-//	    assertTrue(isSquare(1));
-//	    assertFalse(isSquare(3));
-//	    assertFalse(isSquare(22));
-//	    assertFalse(isSquare(143));
-//
-//	  }
-//
-//	@Test
-//	  public void CubeTest() {
-//
-//	    assertTrue(isCube(27));
-//	    assertTrue(isCube(216));
-//	    assertTrue(isCube(729));
-//	    assertTrue(isCube(1));
-//	    assertFalse(isCube(3));
-//	    assertFalse(isCube(22));
-//	    assertFalse(isCube(143));
-//
-//	  }
+	@Test
+	  public void CubeTest() {
+	    assertTrue(isCube(27));
+	    assertTrue(isCube(216));
+	    assertTrue(isCube(729));
+        assertTrue(isCube(1));
+	    assertFalse(isCube(3));
+	    assertFalse(isCube(22));
+	    assertFalse(isCube(143));
+	  }
+	
 	public String multiply(int a, int z) {
 		String mult = a + " x " + z + " = " + (a*z);
 		return mult;
 	}
 	public boolean isPrime(int a) {
-		boolean prime = false;
+		boolean prime = true;
 		for(int i = 2; i<a;i++) {
-			
+			if(a%i==0) {
+				prime = false;
+			}
+			else {
+			}
+			}
+		return prime;
+	}	
+	public boolean isSquare(int i) {
+		boolean square = false;
+		for(int j = 0; j<=i; j++) {
+			if(j*j==i) {
+				square = true;
+			}
 		}
-		
-		
-		
-		return false;
+		return square;
+	}
+	public boolean isCube(int i) {
+		boolean cube = false;
+		for(int j = 0; j<=i; j++) {
+			if(j*j*j == i) {
+				cube = true;
+			}
+		}
+		return cube;
 	}
 	
 	
